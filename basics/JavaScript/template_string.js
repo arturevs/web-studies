af5390487ba95a88718fad9d9ea6ab76c3d4761f
arturevs <artur.rp2001@gmail.com>
2024-09-16 13:45:08 -0300

    Template Strings

---
+++ basics/JavaScript/template_string.js
@@ -0,0 +1,10 @@
+const stringNormal = "Hello Rebeca!";
+const template = 
+`This is my first line
+${stringNormal}`;
+
+console.log(stringNormal);
+console.log(template)
+const up = texto => texto.toUpperCase()
+console.log(`1+1 = ${1+1}`);
+console.log(`Ei, ${up('cuidado')}!`)
