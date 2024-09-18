eb571807ea74646ba26ce8d81924b779e4fb1ac7
arturevs <artur.rp2001@gmail.com>
2024-09-18 08:57:36 -0300

    object study

---
+++ basics/JavaScript/object.js
@@ -0,0 +1,19 @@
+const prod_1 = {}
+prod_1.name = 'cellphone xlr8'
+prod_1.price = 4999.9
+prod_1["cool discounts"] = 0.4 // !!! DON'T USE SPACE IN ATTRIBUTES OR METHODS NOMENCLATURE
+
+console.log(prod_1)
+
+const prod_2 = {
+    name: "polo t-shirt",
+    price: 79.9,
+    obj: {
+        count : 1,
+        obj: {
+            count: 2,
+        }
+    }
+}
+
+console.log(prod_2)
